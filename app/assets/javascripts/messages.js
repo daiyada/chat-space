82899ae798efc4fd89d302ee09cbcbb5e4510b72
daiyada <shunsuke07l6@gmail.com>
2019-09-11 13:36:57 +0900

    third-commit

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -1,32 +1,31 @@
 $(function(){
 
   function sendMessage(message){
-    if (message.image != "null"){
-    var html_i = `<div class="chat-main__messages__box__bottom">
-                    <img class="l.chat-main__messages__box__bottom__image" src= ${message.image.url}>
-                    </img>
-                  </div>`
+      if (message.image.url != null){
+        var html_i = `<div class="chat-main__messages__box__bottom">
+                        <img class="l.chat-main__messages__box__bottom__image" src= ${message.image.url}>
+                        </img>
+                      </div>`
+      } else {
+        var html_i =``
       }
-    else{
-    var html_i =``
-    }
-    var html_m = `<div class="chat-main__messages__box">
-                  <div class="chat-main__messages__box__top">
-                    <div class="chat-main__messages__box__top__user">
-                      ${message.user_name}
+      var html_m = `<div class="chat-main__messages__box">
+                    <div class="chat-main__messages__box__top">
+                      <div class="chat-main__messages__box__top__user">
+                        ${message.user_name}
+                      </div>
+                      <div class="chat-main__messages__box__top__date">
+                        ${message.created_at}
+                      </div>
                     </div>
-                    <div class="chat-main__messages__box__top__date">
-                      ${message.created_at}
+                    <div class="chat-main__messages__box__bottom">
+                      <div class="chat-main__messages__box__bottom__text">
+                        ${message.content}
+                      </div>
+                        ${html_i}
                     </div>
-                  </div>
-                  <div class="chat-main__messages__box__bottom">
-                    <div class="chat-main__messages__box__bottom__text">
-                      ${message.content}
-                    </div>
-                      ${html_i}
-                  </div>
-                 </div>`
-    return html_m;
+                  </div>`
+      return html_m;
   }
 
   $("#new_message").on("submit",function(e){
@@ -48,6 +47,7 @@ $(function(){
       $("#message_content").val(" ")
       $(".chat-main__messages").animate({scrollTop: $('.chat-main__messages')[0].scrollHeight}, 'fast');
       $(".new_message__right").prop("disabled", false);
+      $("#new_message")[0].reset();
     })
     .fail(function(){
       alert("エラー");
